53278e5e3228eeaeda534aba94b16a3d432ac699
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-20 20:33:46 +0530

    Updated code files: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import { ThemeProvider } from "@/components/ThemeProvider"; // Import ThemeProvider
 
 import DashboardPage from "./pages/DashboardPage";
 import LoginPage from "./pages/LoginPage";
@@ -16,24 +17,22 @@ const queryClient = new QueryClient();
 
 const App = () => (
 <QueryClientProvider client={queryClient}>
+  <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme"> {/* Wrap with ThemeProvider */}
     <TooltipProvider>
-    <Toaster />
-    <Sonner />
-    <BrowserRouter>
-        <Routes>
-
-
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/password-recovery" element={<PasswordRecoveryPage />} />
-          <Route path="/registration" element={<RegistrationPage />} />
-          {/* catch-all */}
-          <Route path="*" element={<NotFound />} />
-
-
-        </Routes>
-    </BrowserRouter>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/dashboard" element={<DashboardPage />} />
+            <Route path="/password-recovery" element={<PasswordRecoveryPage />} />
+            <Route path="/registration" element={<RegistrationPage />} />
+            {/* catch-all */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+      </BrowserRouter>
     </TooltipProvider>
+  </ThemeProvider>
 </QueryClientProvider>
 );
 
